bbcb1197d99ba661a28db7b0888d42cda0a590d4
Sergei <staroduber@yandex.ru>
2018-12-18 20:52:10 +0400

    1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,59 @@ import Styles from './App.css';
 import Field from './components/Field.jsx';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.whiteCheckers = [565,530,495,460,425,390,355,320,285,250,215,180,145,110];
+    this.blackCheckers = [10,45,80,115,150,185,220,255,290,325,360,395,430,465];
+  }
   state = {
     valueDice1: undefined,
-    valueDice2: undefined
+    valueDice2: undefined,
+    currentChecker: undefined
+  }
+  setDiceUndefined = (dice) => {
+    if (dice === 'dice1') {
+      this.state.valueDice1 = undefined;
+      this.setState({valueDice1: this.state.valueDice1});
+    }
+    if (dice === 'dice2') {
+      this.state.valueDice2 = undefined;
+      this.setState({valueDice2: this.state.valueDice2});
+    }
   }
   getValue =(a,b) => {
     this.state.valueDice1 = a;
     this.state.valueDice2 = b;
     this.setState({valueDice1: a,valueDice2: b })
   }
+  moveChecker = (e) => {
+    if (this.state.valueDice1 !== undefined || this.state.valueDice2 !== undefined) {
+      let allWhiteCheckers = document.querySelectorAll('.white');
+      allWhiteCheckers.forEach((checker) => {checker.style.border = ''});
+      e.target.style.border = '1px solid white';
+      this.state.currentChecker = e.target;
+      this.setState({currentChecker: this.state.currentChecker})
+    }
+  }
+  undefinedCurrentChecker = () => {
+    this.state.currentChecker = undefined;
+    this.setState({currentChecker: this.state.currentChecker});
+  }
+
   render() {
+    const whiteCheckers = this.whiteCheckers.map((item, index) => {
+      return <div key = {index} style = {{top: item +'px'}} className = 'white' onClick = {this.moveChecker} data-location = '0' ></div>
+    });
+    const blackCheckers = this.blackCheckers.map((item, index) => {
+      return <div key = {index} style = {{top: item + 'px'}} className = 'black'></div>
+    });
+
     return (
       <>
       <Dice getValue = {this.getValue}/>
-      <Field/>
+      <Field currentChecker = {this.state.currentChecker} upUndefined = {this.undefinedCurrentChecker} dice1 = {this.state.valueDice1} dice2 = {this.state.valueDice2} setDiceUndefined = {this.setDiceUndefined}/>
+      {whiteCheckers}
+      {blackCheckers}
       </>
     );
   }
--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -5,19 +5,39 @@ class Field extends React.Component {
   constructor(){
     super();
     this.divs = [
-      'whiteHomeStart',
       'cell1','cell2','cell3','cell4','cell5','cell6',
       'cell7','cell8','cell9','cell10','cell11','cell12',
       'blackHomeEnd',
-      'blackHomeStart',
       'cell13','cell14','cell15','cell16','cell17','cell18',
       'cell19','cell20','cell21','cell22','cell23','cell24',
       'whiteHomeEnd'
     ];
   }
+  getCords = (e) => {
+    const moveChecker = () => {
+      const css = getComputedStyle(e.target);
+      this.props.currentChecker.style.top = css.top;
+      this.props.currentChecker.style.left = css.left;
+      this.props.currentChecker.style.border = '';
+      this.props.currentChecker.setAttribute ('data-location' , e.target.getAttribute('data-place'));
+      this.props.upUndefined();
+    }
+    if (this.props.currentChecker !== undefined) {
+      let currentPosition = +this.props.currentChecker.getAttribute('data-location'),
+      wantedPosition = +e.target.getAttribute('data-place');
+      if ((wantedPosition - currentPosition) === (+this.props.dice1)) {
+        moveChecker();
+        this.props.setDiceUndefined('dice1');
+      }
+      if ((wantedPosition - currentPosition) === (+this.props.dice2))  {
+        moveChecker();
+        this.props.setDiceUndefined('dice2')
+      }
+    }
+  }
   render() {
     const cells = this.divs.map((item, index) => {
-      return <div id = {item} key = {index} className = 'cells'></div>;
+      return <div id = {item} data-place = {(item === 'blackHomeEnd' || item === 'whiteHomeEnd')? null : item.substr(4)} key = {index} className = 'cells' onClick = {this.getCords}></div>;
     });
       return (
         <>
